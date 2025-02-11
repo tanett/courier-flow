ea5082d343eb12415c0da4f6184ec7f1688b0681
ta-nett@mail.ru <klmnopgg>
2025-02-11 11:15:41 +0100

    change base currency.tsx

--- src/app/config/currency.tsx
+++ src/app/config/currency.tsx
@@ -1,8 +1,9 @@
-export const defaultCurrency = 'AMD'
+export const defaultCurrency = 'UZS'
 
 export const systemCurrencyObj: Record<string, string> = {
     USD : '$',
     EUR : '€',
     AMD : '֏',
     RUB : '₽',
+    UZS : 'sum'
 }
